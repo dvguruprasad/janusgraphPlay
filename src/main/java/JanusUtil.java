3ba31e3f24683e3fc524f63d01827ae2bb40ef52
Thejas <thejasb99@gmail.com>
2018-03-06 22:05:54 +0530

    Add #enableIndex to JanusUtil, which enables the given index

--- src/main/java/JanusUtil.java
+++ src/main/java/JanusUtil.java
@@ -2,9 +2,14 @@ import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphException;
+import org.janusgraph.core.schema.JanusGraphIndex;
 import org.janusgraph.core.schema.JanusGraphManagement;
+import org.janusgraph.core.schema.SchemaAction;
+import org.janusgraph.core.schema.SchemaStatus;
+import org.janusgraph.graphdb.database.management.ManagementSystem;
 
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class JanusUtil {
     /*
@@ -38,6 +43,31 @@ public class JanusUtil {
         }
         graph.tx().commit();
         graph.tx().close();
+    }
+
+    /*
+     *
+     * enableIndex enables an already installed index. It does not create the index
+     *
+     */
+    public void enableIndex(JanusGraph graph, String indexName) {
+        try{
+            // Block until the SchemaStatus transitions from INSTALLED to REGISTERED
+            ManagementSystem.awaitGraphIndexStatus(graph, indexName).status(SchemaStatus.REGISTERED).call();
+
+            // Enable the index using JanusGraphManagement
+            JanusGraphManagement mgmt = graph.openManagement();
+            JanusGraphIndex index = mgmt.getGraphIndex(indexName);
+            mgmt.updateIndex(index, SchemaAction.ENABLE_INDEX).get();
+            mgmt.commit();
+            graph.tx().commit();
+
+            // Block until the SchemaStatus transitions from REGISTERED to ENABLED
+            ManagementSystem.awaitGraphIndexStatus(graph, indexName).status(SchemaStatus.ENABLED).call();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
